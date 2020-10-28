921997ddada6c12e40113d0a8702134304001bd0
George Flores <georgeflores886@gmail.com>
2020-10-28 15:16:09 -0400

    created server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 8000
+app.use(express.static('public'))
+app.use(express.json())
+
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
+app.listen(PORT, function () {
+    console.log(`Listening on PORT ${PORT}`)
+})
