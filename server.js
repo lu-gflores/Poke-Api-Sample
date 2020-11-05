3ee0839993c8a5bb56fd98b6d799596c8cf84876
George Flores <georgeflores886@gmail.com>
2020-11-05 16:12:37 -0500

    created index

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 const routes = require('./controllers/api-routes.js')
+const rendeRoutes = require('./controllers/render-routes.js')
 app.use(routes)
+app.use(rendeRoutes)
 
 app.listen(PORT, function () {
     console.log(`Listening on PORT ${PORT}`)
