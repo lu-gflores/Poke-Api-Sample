1722ed68cfb1f0ffdaf2df6c9da7cd43da377b44
George Flores <georgeflores886@gmail.com>
2020-12-15 15:35:41 -0500

    added store and updated detail action

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -1,9 +1,16 @@
 import axios from 'axios'
 import {pokemonDetailsURL, searchPokemonURL} from '../api'
 
-export const loadDetails = id = async (dispatch) => {
+export const loadDetails = (id) => async (dispatch) => {
     dispatch({
         type: 'LOADING_DETAIL'
     });
-    const pokeData = await axios.get(pokemonDetailsURL())
+    const pokeData = await axios.get(pokemonDetailsURL(id))
+    dispatch({
+        type: 'GET_DETAIL',
+        payload: {
+            pokemon: pokeData.data
+        }
+    })
+
 }
